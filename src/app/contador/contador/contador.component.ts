2db2cecc7fee9ef8d4c734cc012bcd3f121b8490
OdMorales <OdMorales@DESKTOP-MH402MR>
2021-08-14 09:39:02 -0500

    primer commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-contador',
+    template: `
+        <h1> {{ title }} </h1>
+
+        <h3>La base es: <Strong> {{base}} </Strong></h3>
+
+        <button (click) = "acomular(+base)"> +{{base}} </button>
+
+        <span> {{ numero }} </span>
+
+        <button (click) = "acomular(-base)"> -{{base}} </button>
+    `
+})
+export class ContadorComponent {
+    
+    title: string = 'Contador App';
+    numero: number = 10;
+
+    base: number = 5;
+
+    acomular(valor: number){
+        this.numero += valor;
+    }
+}
